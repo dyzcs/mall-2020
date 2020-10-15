b73451eebfbf2e68e0fbeb62b86006ef010cb811
dyzcs <deng.kinger@gmail.com>
2020-10-15 14:23:41 +0800

    Update LogUpLoader.java

--- mall-mocker/src/main/java/com/dyzcs/uploader/LogUpLoader.java
+++ mall-mocker/src/main/java/com/dyzcs/uploader/LogUpLoader.java
@@ -1,6 +1,5 @@
 package com.dyzcs.uploader;
 
-import java.io.IOException;
 import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
@@ -16,23 +15,28 @@ public class LogUpLoader {
 
             HttpURLConnection conn = (HttpURLConnection) url.openConnection();
 
-            // 设置请求方式为 post
+            //设置请求方式为post
             conn.setRequestMethod("POST");
 
-            // 时间头用来供 server 进行时钟校对
+            //时间头用来供server进行时钟校对的
             conn.setRequestProperty("clientTime", System.currentTimeMillis() + "");
 
-            // 允许上传数据
+            //允许上传数据
             conn.setDoOutput(true);
 
-            // 输出流
+            //设置请求的头信息,设置内容类型为JSON
+            conn.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+
+            System.out.println("upload" + log);
+
+            //输出流
             OutputStream out = conn.getOutputStream();
             out.write(("logString=" + log).getBytes());
             out.flush();
             out.close();
             int code = conn.getResponseCode();
             System.out.println(code);
-        } catch (IOException e) {
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
