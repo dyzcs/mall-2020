e2b6a42020c1af92d8039f4f151b7272371397ef
dyzcs <deng.kinger@gmail.com>
2020-10-15 14:14:30 +0800

    add springboot

---
+++ mall-logger/src/test/java/com/dyzcs/malllogger/MallLoggerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dyzcs.malllogger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallLoggerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
