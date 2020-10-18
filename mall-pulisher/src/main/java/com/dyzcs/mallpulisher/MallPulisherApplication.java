78106c484e60bd245d9bdf3c6c77b495464293ac
dyzcs <deng.kinger@gmail.com>
2020-10-18 15:38:54 +0800

    add mall-pulisher
    phoenix 数据发布接口

---
+++ mall-pulisher/src/main/java/com/dyzcs/mallpulisher/MallPulisherApplication.java
@@ -0,0 +1,13 @@
+package com.dyzcs.mallpulisher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MallPulisherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MallPulisherApplication.class, args);
+	}
+
+}
---
+++ mall-pulisher/src/test/java/com/dyzcs/mallpulisher/MallPulisherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dyzcs.mallpulisher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallPulisherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
