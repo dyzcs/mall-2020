906bca96b01695f3feacc795cb76fcd535863a11
dyzcs <deng.kinger@gmail.com>
2020-10-19 13:54:56 +0800

    日常更新

--- mall-publisher/src/main/java/com/dyzcs/mallpulisher/mapper/DauMapper.java
+++ mall-publisher/src/main/java/com/dyzcs/mallpulisher/mapper/DauMapper.java
@@ -1,8 +1,13 @@
 package com.dyzcs.mallpulisher.mapper;
 
+import java.util.List;
+import java.util.Map;
+
 /**
  * Created by Administrator on 2020/10/18.
  */
 public interface DauMapper {
     Integer selectDauTotal(String date);
+
+    List<Map> selectDauTotalHourMap(String date);
 }
