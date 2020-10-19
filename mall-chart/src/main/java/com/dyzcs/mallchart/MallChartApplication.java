34b2c8050dc5e145e095dbdd7e70fad5272c9e44
dyzcs <deng.kinger@gmail.com>
2020-10-19 14:24:03 +0800

    mall-chart
    前端展示框架

---
+++ mall-chart/src/main/java/com/dyzcs/mallchart/MallChartApplication.java
@@ -0,0 +1,13 @@
+package com.dyzcs.mallchart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MallChartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MallChartApplication.class, args);
+	}
+
+}
